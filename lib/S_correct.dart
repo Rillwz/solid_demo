7720e75a7b494a945516b66d7936a7489530e9d8
Yusril Ihsanul Alim <73714271+Rillwz@users.noreply.github.com>
2021-08-22 13:40:11 +0700

    Added Single Responsibility Example

--- lib/S_correct.dart
+++ lib/S_correct.dart
@@ -1,23 +1,28 @@
+// Correct Example
+
 class Car {
-  // Wrong Example
+  late AirConditioner airConditioner;
+  late Radio radio;
 
   void turnOnCar() {
     // do something;
   }
-
   void turnOffCar() {
     // do something;
   }
+}
 
+class AirConditioner {
   void turnOnAirConditioner() {
     // do something;
   }
-
   void turnOffAirConditioner() {
     // do something;
   }
+}
 
+class Radio {
   void playMusic() {
-    // do something;
+    // ..
   }
 }
