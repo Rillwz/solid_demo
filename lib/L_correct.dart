4747628974df3450b0e0668c0631c9c8798a89c5
Yusril Ihsanul Alim <73714271+Rillwz@users.noreply.github.com>
2021-08-22 13:49:55 +0700

    L-iskov Substituion Principle

---
+++ lib/L_correct.dart
@@ -0,0 +1,22 @@
+// Right Example
+
+class Duck {
+  void swim() {
+    // do something
+  }
+
+  void quack() {
+    // do something
+  }
+
+  void eat() {
+    // do something
+  }
+}
+
+class Malard extends Duck {
+  @override
+  void eat() {
+    // do Malard way of weating
+  }
+}
