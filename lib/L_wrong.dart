05d360c722e7caecdc5fe0afe4685905e4ff5936
Yusril Ihsanul Alim <73714271+Rillwz@users.noreply.github.com>
2021-08-22 13:47:14 +0700

    L-iskov Substituion Principle

---
+++ lib/L_wrong.dart
@@ -0,0 +1,22 @@
+// WRONG EXAMPLE
+
+class Duck {
+  void swim() {
+    // do nothing ..
+  }
+
+  void quack() {
+    // do nothing ..
+  }
+
+  void eat() {
+    // do nothing ..
+  }
+}
+
+class RubberDuck extends Duck {
+  @override
+  void eat() {
+    // do nothing ..
+  }
+}
