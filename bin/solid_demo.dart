d0b1165e567a4221fb6a70185c3b713cc58fe7b8
Yusril Ihsanul Alim <73714271+Rillwz@users.noreply.github.com>
2021-08-22 09:48:03 +0700

    first commit

---
+++ bin/solid_demo.dart
@@ -0,0 +1,8 @@
+
+void main(List<String> arguments) {
+// S - ingle Responsibility Principle
+// O-pen / Closed Principle
+// L-iskov Substituion Principle
+// I-nterface Segregation Principle
+// D-ependency Inversion Principle
+}
---
+++ lib/S_wrong.dart
@@ -0,0 +1,23 @@
+class Car {
+  // Wrong Example
+
+  void turnOnCar() {
+    // do something;
+  }
+
+  void turnOffCar() {
+    // do something;
+  }
+
+  void turnOnAirConditioner() {
+    // do something;
+  }
+
+  void turnOffAirConditioner() {
+    // do something;
+  }
+
+  void playMusic() {
+    // ...
+  }
+}
